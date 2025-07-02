6d65fedada86240a14241d8b6a8620acc4af3953
Nicolas Mota <nicolasmotabreu@gmail.com>
2025-07-02 10:48:03 -0300

    Agora o arquivo exemplo é criado na pasta que o usuario desejar, evitando conflito de diretório.

--- services.py
+++ services.py
@@ -55,7 +55,6 @@ def criarAFD(conteudo):
     return afd
 
 def selecionarArquivo():
-    filetypes = (("Arquivos de texto", "*.txt"))
     from tkinter import filedialog
     from tkinter import messagebox
     path = filedialog.askopenfile(title="Abrir arquivo txt", filetypes=[("texto", "*.txt")])
@@ -74,12 +73,10 @@ def selecionarArquivo():
 
 def criarExemplo():
 
-    from pathlib import Path
+    from tkinter import filedialog
     from tkinter import messagebox
 
-    path = Path.home() / "Desktop"
-
-    exemplo = open(f"{path}\\exemplo.txt", "w")
-    exemplo.write("alfabeto:a,b,c,d\nestados:q0,q1,q2\ninicial:q0\nfinais:q1,q2\ntransicoes\nq0,q1,a\nq0,q0,b\nq1,q0,a\nq1,q2,b\nq2,q1,a\nq2,q2,b")
-        
-    messagebox.showinfo(title="Exemplo .txt criado com sucesso!", message="O exemplo de .txt contendo uma AFD foi criado na sua area de trabalho.")
+    diretorioArquivo = filedialog.askdirectory(title ="Selecione a pasta que quer o exemplo")
+    exemplo = open(f"{diretorioArquivo}\\exemplo.txt", "w")
+    exemplo.write("alfabeto:a,b,c,d\nestados:q0,q1,q2\ninicial:q0\nfinais:q1,q2\ntransicoes\nq0,q1,a\nq0,q0,b\nq1,q0,a\nq1,q2,b\nq2,q1,a\nq2,q2,b")  
+    messagebox.showinfo(title="Exemplo .txt criado com sucesso!", message="O exemplo de .txt contendo uma AFD foi criado na pasta selecionada.")
